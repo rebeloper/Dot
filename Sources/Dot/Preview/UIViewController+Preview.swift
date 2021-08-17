d62bc67af524e97ebc713275e37281f9b444ab98
Alex Nagy <therebeloper@gmail.com>
2021-08-17 11:06:58 +0300

    Preview

---
+++ Sources/Dot/Preview/UIViewController+Preview.swift
@@ -0,0 +1,34 @@
+//
+//  UIViewController+Preview.swift
+//  Dot
+//
+//  Created by Alex Nagy on 17.08.2021.
+//
+
+import UIKit
+
+#if DEBUG && canImport(SwiftUI)
+import SwiftUI
+
+public extension UIViewController {
+    private struct UIPreview: UIViewControllerRepresentable {
+        // this variable is used for injecting the current view controller
+        let viewController: UIViewController
+
+        func makeUIViewController(context: Context) -> UIViewController {
+            return viewController
+        }
+
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        }
+    }
+    
+    /// Creates a Preview View from a UIViewController
+    /// - Returns: a view that can be used for Preview
+    func toPreview() -> some View {
+        // inject self (the current view controller) for the preview
+        UIPreview(viewController: self)
+    }
+}
+#endif
+
