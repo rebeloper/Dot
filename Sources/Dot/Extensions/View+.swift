d976a6739a47e356dfe0dc40cf7a3739ea536a0a
Alex Nagy <therebeloper@gmail.com>
2021-08-27 11:25:41 +0300

    onTapGestureForced

--- Sources/Dot/Extensions/View+.swift
+++ Sources/Dot/Extensions/View+.swift
@@ -121,4 +121,44 @@ public extension View {
             .navigationTitle(title)
     }
     
+    /// Adds an action to perform when this view recognizes a tap gesture. Works with ``Spacer``.
+    ///
+    /// Use this method to perform a specific `action` when the user clicks or
+    /// taps on the view or container `count` times.
+    ///
+    /// > Note: If you are creating a control that's functionally equivalent
+    /// to a ``Button``, use ``ButtonStyle`` to create a customized button
+    /// instead.
+    ///
+    /// In the example below, the color of the heart images changes to a random
+    /// color from the `colors` array whenever the user clicks or taps on the
+    /// view twice:
+    ///
+    ///     struct TapGestureExample: View {
+    ///         let colors: [Color] = [.gray, .red, .orange, .yellow,
+    ///                                .green, .blue, .purple, .pink]
+    ///         @State private var fgColor: Color = .gray
+    ///
+    ///         var body: some View {
+    ///             Image(systemName: "heart.fill")
+    ///                 .resizable()
+    ///                 .frame(width: 200, height: 200)
+    ///                 .foregroundColor(fgColor)
+    ///                 .onTapGesture(count: 2, perform: {
+    ///                     fgColor = colors.randomElement()!
+    ///                 })
+    ///         }
+    ///     }
+    ///
+    /// ![A screenshot of a view of a heart.](SwiftUI-View-TapGesture.png)
+    ///
+    /// - Parameters:
+    ///    - count: The number of taps or clicks required to trigger the action
+    ///      closure provided in `action`. Defaults to `1`.
+    ///    - action: The action to perform.
+    func onTapGestureForced(count: Int = 1, perform action: @escaping () -> Void) -> some View {
+        self
+            .contentShape(Rectangle())
+            .onTapGesture(count:count, perform:action)
+    }
 }
