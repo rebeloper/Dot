689a43de388d3ff4c6839f55665a5ab65121cf87
Alex Nagy <therebeloper@gmail.com>
2021-09-01 21:19:38 +0300

    LoremPicsum

--- Sources/Dot/Assets/MediaCredits.swift
+++ Sources/Dot/Assets/MediaCredits.swift
@@ -6,8 +6,23 @@
 //
 
 /*
+ ade-tunji-B0q7eBuXKjA-unsplash
+ https://unsplash.com/photos/B0q7eBuXKjA?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+ 
+ anastasia-vityukova-ZRfSpvIh-FA-unsplash
+ https://unsplash.com/photos/ZRfSpvIh-FA?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+ 
+ angelo-pari-X5dYIZj24sQ-unsplash
+ https://unsplash.com/photos/X5dYIZj24sQ?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+ 
+ courtney-cook-TSZo17r3m0s-unsplash
+ https://unsplash.com/photos/TSZo17r3m0s?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+ 
  damien-schnorhk-ccg5KqHcIlU-unsplash
  https://unsplash.com/photos/ccg5KqHcIlU?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+ 
+ darshan-patel-QJEVpydulGs-unsplash
+ https://unsplash.com/photos/QJEVpydulGs?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
  
  devon-divine-8bIFt7xq_uU-unsplash
  https://unsplash.com/photos/8bIFt7xq_uU?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
@@ -18,11 +33,17 @@
  ehimetalor-akhere-unuabona-Vbk36H9YNLQ-unsplash
  https://unsplash.com/photos/Vbk36H9YNLQ?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
  
+ evilicio-inc-zo9-6ofZ91Y-unsplash
+ https://unsplash.com/photos/zo9-6ofZ91Y?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+ 
  florian-wehde-cgVrmivyQvI-unsplash
  https://unsplash.com/photos/cgVrmivyQvI?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
  
  hossein-beygi-P6XQbfknFR8-unsplash
  https://unsplash.com/photos/P6XQbfknFR8?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+ 
+ idowu-emmanuel-pzclzArQSA4-unsplash
+ https://unsplash.com/photos/pzclzArQSA4?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
  
  ismail-zabalawi-WX39Ass0zDs-unsplash
  https://unsplash.com/photos/WX39Ass0zDs?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
@@ -30,11 +51,20 @@
  israel-andrade-YI_9SivVt_s-unsplash
  https://unsplash.com/photos/YI_9SivVt_s?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
  
+ karsten-winegeart-qYO_mjVcLpg-unsplash
+ https://unsplash.com/photos/qYO_mjVcLpg?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+ 
  lerone-pieters-vy4Yg_d-3DI-unsplash
  https://unsplash.com/photos/vy4Yg_d-3DI?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
  
  maksym-tymchyk-bGOBoZorNoQ-unsplash
  https://unsplash.com/photos/bGOBoZorNoQ?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+ 
+ mika-baumeister-k7ppbDckDwQ-unsplash
+ https://unsplash.com/photos/k7ppbDckDwQ?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+ 
+ mos-sukjaroenkraisri-jz8AmJDyhig-unsplash
+ https://unsplash.com/photos/jz8AmJDyhig?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
  
  quinton-coetzee-Lf94850dM14-unsplash
  https://unsplash.com/photos/Lf94850dM14?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
--- Sources/Dot/Utils/LoremPicsum.swift
+++ Sources/Dot/Utils/LoremPicsum.swift
@@ -7,7 +7,9 @@
 
 import SwiftUI
 
+/// Adds Dummy Images
 public struct LoremPicsum {
+    
     public static var foodAndDrink: Image {
         Image("slashio-photography-bKAWpR4y5nU-unsplash", bundle: Bundle.module)
     }
@@ -40,10 +42,6 @@ public struct LoremPicsum {
         Image("dmitriy-frantsev-zbafP5GeL0Q-unsplash", bundle: Bundle.module)
     }
     
-    public static var interior: Image {
-        Image("dmitriy-frantsev-zbafP5GeL0Q-unsplash", bundle: Bundle.module)
-    }
-    
     public static var street: Image {
         Image("lerone-pieters-vy4Yg_d-3DI-unsplash", bundle: Bundle.module)
     }
@@ -66,5 +64,87 @@ public struct LoremPicsum {
     
     public static var history: Image {
         Image("ismail-zabalawi-WX39Ass0zDs-unsplash", bundle: Bundle.module)
+    }
+    
+    public static var person0: Image {
+        Image("mika-baumeister-k7ppbDckDwQ-unsplash", bundle: Bundle.module)
+    }
+    
+    public static var person1: Image {
+        Image("darshan-patel-QJEVpydulGs-unsplash", bundle: Bundle.module)
+    }
+    
+    public static var person2: Image {
+        Image("karsten-winegeart-qYO_mjVcLpg-unsplash", bundle: Bundle.module)
+    }
+    
+    public static var person3: Image {
+        Image("mos-sukjaroenkraisri-jz8AmJDyhig-unsplash", bundle: Bundle.module)
+    }
+    
+    public static var person4: Image {
+        Image("idowu-emmanuel-pzclzArQSA4-unsplash", bundle: Bundle.module)
+    }
+    
+    public static var person5: Image {
+        Image("evilicio-inc-zo9-6ofZ91Y-unsplash", bundle: Bundle.module)
+    }
+    
+    public static var person6: Image {
+        Image("ade-tunji-B0q7eBuXKjA-unsplash", bundle: Bundle.module)
+    }
+    
+    public static var person7: Image {
+        Image("anastasia-vityukova-ZRfSpvIh-FA-unsplash", bundle: Bundle.module)
+    }
+    
+    public static var person8: Image {
+        Image("courtney-cook-TSZo17r3m0s-unsplash", bundle: Bundle.module)
+    }
+    
+    public static var person9: Image {
+        Image("angelo-pari-X5dYIZj24sQ-unsplash", bundle: Bundle.module)
+    }
+    
+    public static let images = [
+        foodAndDrink,
+        backToSchool,
+        nature,
+        architecture,
+        business,
+        fashion,
+        health,
+        interior,
+        street,
+        technology,
+        texture,
+        travel,
+        athletics,
+        history
+    ]
+    
+    public static func randomImage() -> Image {
+        images.randomElement() ?? foodAndDrink
+    }
+    
+    public static let people = [
+        person0,
+        person1,
+        person2,
+        person3,
+        person4,
+        person5,
+        person6,
+        person7,
+        person8,
+        person9
+    ]
+    
+    public static func randomPerson() -> Image {
+        people.randomElement() ?? person0
+    }
+    
+    public static func random() -> Image {
+        people.append(contentsOf: images).randomElement() ?? person0
     }
 }
