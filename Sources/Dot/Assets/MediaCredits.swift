6fc99aea432e29725ae6c3783ee2eadf7faf4990
Alex Nagy <therebeloper@gmail.com>
2021-09-01 14:16:18 +0300

    LoremPicsum

--- Sources/Dot/Assets/MediaCredits.swift
+++ Sources/Dot/Assets/MediaCredits.swift
@@ -9,9 +9,43 @@
  damien-schnorhk-ccg5KqHcIlU-unsplash
  https://unsplash.com/photos/ccg5KqHcIlU?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
  
+ devon-divine-8bIFt7xq_uU-unsplash
+ https://unsplash.com/photos/8bIFt7xq_uU?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+ 
+ dmitriy-frantsev-zbafP5GeL0Q-unsplash
+ https://unsplash.com/photos/zbafP5GeL0Q?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+ 
+ ehimetalor-akhere-unuabona-Vbk36H9YNLQ-unsplash
+ https://unsplash.com/photos/Vbk36H9YNLQ?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+ 
+ florian-wehde-cgVrmivyQvI-unsplash
+ https://unsplash.com/photos/cgVrmivyQvI?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+ 
+ hossein-beygi-P6XQbfknFR8-unsplash
+ https://unsplash.com/photos/P6XQbfknFR8?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+ 
+ ismail-zabalawi-WX39Ass0zDs-unsplash
+ https://unsplash.com/photos/WX39Ass0zDs?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+ 
+ israel-andrade-YI_9SivVt_s-unsplash
+ https://unsplash.com/photos/YI_9SivVt_s?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+ 
+ lerone-pieters-vy4Yg_d-3DI-unsplash
+ https://unsplash.com/photos/vy4Yg_d-3DI?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+ 
+ maksym-tymchyk-bGOBoZorNoQ-unsplash
+ https://unsplash.com/photos/bGOBoZorNoQ?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+ 
  quinton-coetzee-Lf94850dM14-unsplash
  https://unsplash.com/photos/Lf94850dM14?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
  
  slashio-photography-bKAWpR4y5nU-unsplash
  https://unsplash.com/photos/bKAWpR4y5nU?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+ 
+ taiki-ishikawa-cXjFsX5UvGw-unsplash
+ https://unsplash.com/photos/cXjFsX5UvGw?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+ 
+ tyler-nix-HmVQh_EQJhY-unsplash
+ https://unsplash.com/photos/HmVQh_EQJhY?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+ 
  */
--- Sources/Dot/Utils/LoremPicsum.swift
+++ Sources/Dot/Utils/LoremPicsum.swift
@@ -19,4 +19,52 @@ public struct LoremPicsum {
     public static var nature: Image {
         Image("damien-schnorhk-ccg5KqHcIlU-unsplash", bundle: Bundle.module)
     }
+    
+    public static var architecture: Image {
+        Image("devon-divine-8bIFt7xq_uU-unsplash", bundle: Bundle.module)
+    }
+    
+    public static var business: Image {
+        Image("israel-andrade-YI_9SivVt_s-unsplash", bundle: Bundle.module)
+    }
+    
+    public static var fashion: Image {
+        Image("ehimetalor-akhere-unuabona-Vbk36H9YNLQ-unsplash", bundle: Bundle.module)
+    }
+    
+    public static var health: Image {
+        Image("tyler-nix-HmVQh_EQJhY-unsplash", bundle: Bundle.module)
+    }
+    
+    public static var interior: Image {
+        Image("dmitriy-frantsev-zbafP5GeL0Q-unsplash", bundle: Bundle.module)
+    }
+    
+    public static var interior: Image {
+        Image("dmitriy-frantsev-zbafP5GeL0Q-unsplash", bundle: Bundle.module)
+    }
+    
+    public static var street: Image {
+        Image("lerone-pieters-vy4Yg_d-3DI-unsplash", bundle: Bundle.module)
+    }
+    
+    public static var technology: Image {
+        Image("taiki-ishikawa-cXjFsX5UvGw-unsplash", bundle: Bundle.module)
+    }
+    
+    public static var texture: Image {
+        Image("hossein-beygi-P6XQbfknFR8-unsplash", bundle: Bundle.module)
+    }
+    
+    public static var travel: Image {
+        Image("florian-wehde-cgVrmivyQvI-unsplash", bundle: Bundle.module)
+    }
+    
+    public static var athletics: Image {
+        Image("maksym-tymchyk-bGOBoZorNoQ-unsplash", bundle: Bundle.module)
+    }
+    
+    public static var history: Image {
+        Image("ismail-zabalawi-WX39Ass0zDs-unsplash", bundle: Bundle.module)
+    }
 }
