8d7ae8529dbb0ea6dd919a287e086ab4618cefc4
Alex Nagy <therebeloper@gmail.com>
2021-09-01 08:53:01 +0300

    LoremPicsum

---
+++ Sources/Dot/Assets/MediaCredits.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  File
+//
+//  Created by Alex Nagy on 01.09.2021.
+//
+
+/*
+ slashio-photography-bKAWpR4y5nU-unsplash
+ https://unsplash.com/photos/bKAWpR4y5nU?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+
+
+ */
---
+++ Sources/Dot/Utils/LoremPicsum.swift
@@ -0,0 +1,14 @@
+//
+//  LoremPicsum.swift
+//  LoremPicsum
+//
+//  Created by Alex Nagy on 01.09.2021.
+//
+
+import SwiftUI
+
+public struct LoremPicsum {
+   public var foodAndDrink: Image {
+       Image("slashio-photography-bKAWpR4y5nU-unsplash", bundle: Bundle.module)
+   }
+}
