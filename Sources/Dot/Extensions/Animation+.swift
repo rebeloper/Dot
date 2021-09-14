71a3eb88e3b7c789d917d2f8b1c0cae177a6ba03
Alex Nagy <therebeloper@gmail.com>
2021-09-14 11:42:42 +0300

    SplashScreen

--- Sources/Dot/Extensions/Animation+.swift
+++ Sources/Dot/Extensions/Animation+.swift
@@ -8,8 +8,8 @@
 import SwiftUI
 
 public extension Animation {
-    func `repeatForever`(while expression: Bool, autoreverses: Bool = true) -> Animation {
-        if expression {
+    func `repeatForever`(while expression: Binding<Bool>, autoreverses: Bool = true) -> Animation {
+        if expression.wrappedValue {
             return self.repeatForever(autoreverses: autoreverses)
         } else {
             return self
--- Sources/Dot/SplashScreen/SplashScreen.swift
+++ Sources/Dot/SplashScreen/SplashScreen.swift
@@ -7,74 +7,67 @@
 
 import SwiftUI
 
-public struct SplashScreen<Content: View, Background: View>: View {
+struct SplashScreen<Content: View, Background: View>: View {
     
     @Binding private var isActive: Bool
     private var scale: CGFloat
     private var delay: CGFloat
-    private var pulseScale: CGFloat
-    private var animationDuration: CGFloat
     private var animation: SplashScreenAnimation
+    private var animationValue: CGFloat
+    private var animationDuration: CGFloat
     private var content: () -> Content
     private var background: () -> Background
     
     @State private var shouldAnimate = false
     @State private var shouldClose = false
     @State private var pulsates = true
+    @State private var animationState = true
     
-    /// Splash screen; add this to the root view of your app
-    /// - isActive: is the splash screen active
-    /// - scale: the scale of the content when the splash view is dismissed
-    /// - delay: the delay of animation when the splash view is dismissed
-    /// - pulseScale: the value the pulse animation has
-    /// - animationDuration: the duration of the animation
-    /// - animation: the pulse animation
-    /// - content: the content that is centered in the splash screen
-    /// - background: the background of the splash screen
-    public init(isActive: Binding<Bool>,
-                scale: CGFloat = 20,
-                delay: CGFloat = 0.3,
-                pulseScale: CGFloat = 0.95,
-                animationDuration: CGFloat = 1,
-                animation: SplashScreenAnimation = .scale,
-                @ViewBuilder content: @escaping () -> Content,
-                @ViewBuilder background: @escaping () -> Background) {
+    init(isActive: Binding<Bool>,
+         scale: CGFloat = 20,
+         delay: CGFloat = 0.3,
+         animation: SplashScreenAnimation = .scale,
+         animationValue: CGFloat = 0.95,
+         animationDuration: CGFloat = 1,
+         @ViewBuilder content: @escaping () -> Content,
+         @ViewBuilder background: @escaping () -> Background) {
         self._isActive = isActive
         self.scale = scale
         self.delay = delay
-        self.pulseScale = pulseScale
-        self.animationDuration = animationDuration
         self.animation = animation
+        self.animationValue = animationValue
+        self.animationDuration = animationDuration
         self.content = content
         self.background = background
     }
     
-    public var body: some View {
+    var body: some View {
         ZStack {
             background().ignoresSafeArea()
             
             content()
                 .scaleEffect(shouldClose ? scale : 1)
                 .if(animation == .scale, transform: { view in
-                    view.scaleEffect(pulsates ? pulseScale : 1)
+                    view.scaleEffect(animationState ? animationValue : 1)
                 })
                 .if(animation == .fadeInOut, transform: { view in
-                    view.opacity(pulsates ? Double(pulseScale) : 1)
+                    view.opacity(animationState ? Double(animationValue) : 1)
                 })
                 .if(animation == .wiggle, transform: { view in
-                    view.rotationEffect(Angle(degrees: Double(pulsates ? pulseScale : -pulseScale)))
+                    view.rotationEffect(Angle(degrees: Double(animationState ? animationValue : -animationValue)))
                 })
                 .if(animation == .moveHorizontally, transform: { view in
                     view
-                        .offset(x: pulsates ? pulseScale : -pulseScale)
+                        .offset(x: animationState ? animationValue : -animationValue)
                 })
                 .if(animation == .moveVertically, transform: { view in
                     view
-                        .offset(y: pulsates ? pulseScale : -pulseScale)
+                        .offset(y: animationState ? animationValue : -animationValue)
                 })
                 .onChange(of: isActive, perform: { isActive in
                     if !isActive {
                         DispatchQueue.main.async {
+                            pulsates = false
                             withAnimation {
                                 shouldAnimate = true
                             }
@@ -94,40 +87,35 @@ public struct SplashScreen<Content: View, Background: View>: View {
     }
     
     private func pulsate() {
-        withAnimation(.easeInOut(duration: Double(animationDuration)).repeatForever(while: !shouldAnimate)) {
-            if shouldAnimate { return }
-            pulsates.toggle()
+        withAnimation(.easeInOut(duration: animationDuration).repeatForever(while: $pulsates)) {
+            animationState.toggle()
         }
     }
 }
 
-public enum SplashScreenAnimation {
+enum SplashScreenAnimation {
     case none, scale, fadeInOut, wiggle, moveHorizontally, moveVertically
 }
 
 
-public extension View {
-    
-    /// Splash screen; add this to the root view of your app
-    /// - isActive: is the splash screen active
-    /// - scale: the scale of the content when the splash view is dismissed
-    /// - delay: the delay of animation when the splash view is dismissed
-    /// - pulseScale: the value the pulse animation has
-    /// - animationDuration: the duration of the animation
-    /// - animation: the pulse animation
-    /// - content: the content that is centered in the splash screen
-    /// - background: the background of the splash screen
+extension View {
     func splashScreen<Content: View, Background: View>(isActive: Binding<Bool>,
                                                        scale: CGFloat = 20,
                                                        delay: CGFloat = 0.3,
-                                                       pulseScale: CGFloat = 0.95,
-                                                       animationDuration: CGFloat = 1,
                                                        animation: SplashScreenAnimation = .scale,
+                                                       animationValue: CGFloat = 0.95,
+                                                       animationDuration: CGFloat = 1,
                                                        @ViewBuilder content: @escaping () -> Content,
-                                                       @ViewBuilder background: @escaping () -> Background) -> some View {
+                      @ViewBuilder background: @escaping () -> Background) -> some View {
         self.overlay(
-            SplashScreen(isActive: isActive, scale: scale, delay: delay, pulseScale: pulseScale, animationDuration: animationDuration, animation: animation, content: content, background: background)
+            SplashScreen(isActive: isActive,
+                         scale: scale,
+                         delay: delay,
+                         animation: animation,
+                         animationValue: animationValue,
+                         animationDuration: animationDuration,
+                         content: content,
+                         background: background)
         )
     }
 }
-
