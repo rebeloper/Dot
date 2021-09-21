6a0a519b0071300cb11ca89ffd7bf439aee9c8e0
Alex Nagy <therebeloper@gmail.com>
2021-09-21 18:34:20 +0300

    open init

--- Sources/Dot/Utils/ObservedObservableObject.swift
+++ Sources/Dot/Utils/ObservedObservableObject.swift
@@ -12,6 +12,8 @@ import Combine
 /// By default an ``ObservableObject`` synthesizes an ``ObservableObject/objectWillChange-2oa5v`` publisher that emits the changed value before any of its `@Published` properties changes.
 open class ObservedObservableObject: ObservableObject {
     
+    open init() { }
+    
     /// A Set of type-erasing cancellable objects that execute a provided closure when canceled.
     ///
     /// Subscriber implementations can use this type to provide a “cancellation token” that makes it possible for a caller to cancel a publisher, but not to use the ``Subscription`` object to request items.
