759c936a2cee38b53b9268dd3b8147f986334075
Alex Nagy <therebeloper@gmail.com>
2021-09-21 18:27:35 +0300

    ObservedObservableObject

---
+++ Sources/Dot/Utils/ObservedObservableObject.swift
@@ -0,0 +1,21 @@
+//
+//  ObservedObservableObject.swift
+//  
+//
+//  Created by Alex Nagy on 21.09.2021.
+//
+
+import Combine
+
+/// A type of object with a set of ``AnyCancellable``s and with a publisher that emits before the object has changed.
+///
+/// By default an ``ObservableObject`` synthesizes an ``ObservableObject/objectWillChange-2oa5v`` publisher that emits the changed value before any of its `@Published` properties changes.
+public class ObservedObservableObject: ObservableObject {
+    
+    /// A Set of type-erasing cancellable objects that execute a provided closure when canceled.
+    ///
+    /// Subscriber implementations can use this type to provide a “cancellation token” that makes it possible for a caller to cancel a publisher, but not to use the ``Subscription`` object to request items.
+    ///
+    /// An ``AnyCancellable`` instance automatically calls ``Cancellable/cancel()`` when deinitialized.
+    public var cancellables: Set<AnyCancellable> = []
+}
