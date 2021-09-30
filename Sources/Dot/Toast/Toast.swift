4bba497c4216b0fe1b1400bdc268c8e31859b9c5
Alex Nagy <therebeloper@gmail.com>
2021-09-30 12:14:01 +0300

    Toast

--- Sources/Dot/Toast/Toast.swift
+++ Sources/Dot/Toast/Toast.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 public class Toast: ObservableObject {
     
-    @Published public var isPresented: Bool = false
+    @Published private var shouldPresent: Bool = false
+    @Published private var isPresented: Bool = false
     @Published public var config: ToastConfig
     
     /// Creates a Toast
@@ -22,17 +23,25 @@ public class Toast: ObservableObject {
     /// - Parameters:
     ///   - title: title of the Toast
     ///   - message: message of the Toast
-    public func present(_ title: String? = nil, message: String? = nil) {
-        self.config.title = title
-        self.config.message = message
-        withAnimation {
-            isPresented = true
+    ///   - after: delay for the presentation; default is `0.5` to present the Toast only if it was not dismissed before our delay
+    public func present(_ title: String? = nil, message: String? = nil, after: Double = 0.5) {
+        shouldPresent = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + after) {
+            if self.shouldPresent {
+                self.config.title = title
+                self.config.message = message
+                withAnimation {
+                    self.isPresented = true
+                }
+            }
         }
     }
     
     /// Dismisses the Toast
+    /// - Parameter after: delay for the dismiss; default is `0`
     public func dismiss(after: Double = 0) {
         DispatchQueue.main.asyncAfter(deadline: .now() + after) {
+            self.shouldPresent = false
             withAnimation {
                 self.isPresented = false
             }
