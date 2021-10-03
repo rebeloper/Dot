ab113a3cf08b528a952b279fb2655645869eb7a8
Alex Nagy <therebeloper@gmail.com>
2021-10-03 13:44:43 +0300

    Tappable

---
+++ Sources/Dot/Utils/Tappable.swift
@@ -0,0 +1,40 @@
+//
+//  Tappable.swift
+//  
+//
+//  Created by Alex Nagy on 03.10.2021.
+//
+
+import SwiftUI
+
+struct Tappable<Label> : View where Label : View {
+    
+    private let count: Int
+    private let cornerRadius: CGFloat
+    private let style: RoundedCornerStyle
+    private let action: () -> Void
+    private let label: () -> Label
+    
+    /// Creates a tappable and clipped view that displays a custom label.
+    /// - Parameters:
+    ///   - count: The number of taps or clicks required to trigger the action closure provided in `action`. Defaults to `1`
+    ///   - cornerRadius: corner radius. Default is 0
+    ///   - style: rounded corner style. Default is .circular
+    ///   - action: The action to perform
+    ///   - label: A view that describes the purpose of the tappable's `action`.
+    init(count: Int = 1,
+         cornerRadius: CGFloat = 0,
+         style: RoundedCornerStyle = .circular,
+         action: @escaping () -> Void,
+         @ViewBuilder label: @escaping () -> Label) {
+        self.count = count
+        self.cornerRadius = cornerRadius
+        self.style = style
+        self.action = action
+        self.label = label
+    }
+    
+    var body: some View {
+        label().onClippedTapGesture(count: count, cornerRadius: cornerRadius, style: style, perform: action)
+    }
+}
