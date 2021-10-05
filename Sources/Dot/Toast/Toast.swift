f3d396f542efd4e9643d2a38779e775deb768208
Alex Nagy <therebeloper@gmail.com>
2021-10-05 11:01:23 +0300

    wait

--- Sources/Dot/Toast/Toast.swift
+++ Sources/Dot/Toast/Toast.swift
@@ -11,7 +11,7 @@ public class Toast: ObservableObject {
     
     @Published private var shouldPresent: Bool = false
     @Published public var isPresented: Bool = false
-    @Published public var isPresentedDate: Date = Date()
+    @Published private var mayDismiss: Bool = false
     @Published public var config: ToastConfig
     
     /// Creates a Toast
@@ -27,15 +27,18 @@ public class Toast: ObservableObject {
     ///   - throttle: delay for the presentation; default is `0.5` to present the Toast only if it was not dismissed before our delay
     public func present(_ title: String? = nil, message: String? = nil, after: Double = 0, throttle: Double = 0.5) {
         shouldPresent = true
+        mayDismiss = false
         DispatchQueue.main.asyncAfter(deadline: .now() + after + throttle) {
             if self.shouldPresent {
-                self.isPresentedDate = Date()
                 self.config.title = title
                 self.config.message = message
                 withAnimation {
                     self.isPresented = true
                 }
             }
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + after + throttle + 0.5) {
+            mayDismiss = true
         }
     }
     
@@ -44,13 +47,8 @@ public class Toast: ObservableObject {
     ///    - after: delay for the dismiss; default is `0`
     ///    - throttle: delay for the dismiss; default is `0.5` to delay the dismiss of the Toast if it was presented before our delay duration
     public func dismiss(after: Double = 0, throttle: Double = 0.5) {
-        var wait: Double = 0
-        let isPresentedDuration = isPresentedDate.timeIntervalSinceNow + after
-        print("isPresentedDuration: \(isPresentedDuration)")
-        if isPresentedDuration < throttle {
-            wait = 0.5
-        }
-        DispatchQueue.main.asyncAfter(deadline: .now() + after + wait) {
+        let wait: Double = mayDismiss ? 0 : 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + after + throttle + wait) {
             self.shouldPresent = false
             withAnimation {
                 self.isPresented = false
