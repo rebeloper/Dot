ed9f3ab06de6e5de1f37deb033567210193b61f6
Alex Nagy <therebeloper@gmail.com>
2021-11-18 13:11:47 +0200

    PrintInteractionController

---
+++ Sources/Dot/PrintInteractionController/PrintInteractionController.swift
@@ -0,0 +1,48 @@
+//
+//  File.swift
+//  
+//
+//  Created by Alex Nagy on 18.11.2021.
+//
+
+import UIKit
+
+public struct PrintInteractionController {
+    
+    public enum PrintingResult {
+        case success
+        case failure(Error)
+        case userCancelled
+    }
+
+    public static func present(url: URL, printInfo: UIPrintInfo? = nil, completion: ((PrintingResult) -> Void)? = nil) {
+        
+        guard UIPrintInteractionController.isPrintingAvailable else {
+            let error = NSError(domain: "Printing not available", code: 0, userInfo: nil)
+            completion?(.failure(error))
+            return
+        }
+        guard UIPrintInteractionController.canPrint(url) else {
+            let error = NSError(domain: "Cannot print from url: \(url.absoluteString)", code: 1, userInfo: nil)
+            completion?(.failure(error))
+            return
+        }
+        
+        let printController = UIPrintInteractionController()
+        printController.printInfo = printInfo
+        printController.printingItem = url
+        printController.present(animated: true) { _, completed, error in
+            guard let completion = completion else { return }
+            if completed {
+                completion(.success)
+            } else {
+                if let error = error {
+                    completion(.failure(error))
+                } else {
+                    completion(.userCancelled)
+                }
+            }
+        }
+    }
+}
+
