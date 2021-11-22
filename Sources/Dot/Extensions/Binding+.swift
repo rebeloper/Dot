7a19754f96b72293840fbe0f5d924629e384ade9
Alex Nagy <therebeloper@gmail.com>
2021-11-22 14:29:18 +0200

    onChange

---
+++ Sources/Dot/Extensions/Binding+.swift
@@ -0,0 +1,20 @@
+//
+//  Binding+.swift
+//  
+//
+//  Created by Alex Nagy on 22.11.2021.
+//
+
+import SwiftUI
+
+extension Binding {
+    func onChange(_ handler: @escaping (Value) -> Void) -> Binding<Value> {
+        Binding(
+            get: { self.wrappedValue },
+            set: { newValue in
+                self.wrappedValue = newValue
+                handler(newValue)
+            }
+        )
+    }
+}
