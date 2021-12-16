c886ee33f0bfc3f15adbf5a96f2e2f4666c7984d
Alex Nagy <therebeloper@gmail.com>
2021-12-16 15:04:00 +0200

    ToastManager

--- Sources/Dot/Toast/ToastManager.swift
+++ Sources/Dot/Toast/ToastManager.swift
@@ -1,3 +1,70 @@
+////
+////  ToastManager.swift
+////  Dot
+////
+////  Created by Alex Nagy on 10.08.2021.
+////
+//
+//import SwiftUI
+//
+//public class ToastManager: ObservableObject {
+//
+//    @Published var isPresented: Bool = false
+//    @Published var shouldPresent: Bool = false
+//    @Published var mayDismiss: Bool = false
+//    @Published var isThrottled: Bool = false
+//    @Published var config: ToastConfig
+//
+//    /// Creates a ToastManager
+//    /// - Parameter config: ToastManager Configuration
+//    public init(config: ToastConfig = ToastConfig()) {
+//        self.config = config
+//    }
+//
+//    /// Presents a Toast
+//    /// - Parameters:
+//    ///   - title: title of the Toast
+//    ///   - message: message of the Toast
+//    func present(_ title: String? = nil, message: String? = nil) {
+//        DispatchQueue.main.async {
+//            self.shouldPresent = true
+//            self.mayDismiss = false
+//            self.isThrottled = true
+//        }
+//        DispatchQueue.main.asyncAfter(deadline: .now() + config.throttle) {
+//            self.isThrottled = false
+//            if self.shouldPresent {
+//                self.config.title = title
+//                self.config.message = message
+//                withAnimation {
+//                    self.isPresented = true
+//                }
+//            }
+//        }
+//        DispatchQueue.main.asyncAfter(deadline: .now() + config.throttle + config.minPresentedTime) {
+//            self.mayDismiss = true
+//        }
+//    }
+//
+//    /// Dismisses the Toast
+//    func dismiss() {
+//        if isThrottled {
+//            DispatchQueue.main.async {
+//                self.shouldPresent = false
+//            }
+//        } else {
+//            let minPresentedTime: Double = mayDismiss ? 0 : config.minPresentedTime
+//            DispatchQueue.main.asyncAfter(deadline: .now() + minPresentedTime) {
+//                withAnimation {
+//                    self.isPresented = false
+//                }
+//            }
+//        }
+//    }
+//
+//}
+//
+
 //
 //  ToastManager.swift
 //  Dot
@@ -6,14 +73,16 @@
 //
 
 import SwiftUI
+import Combine
 
 public class ToastManager: ObservableObject {
     
     @Published var isPresented: Bool = false
     @Published var shouldPresent: Bool = false
-    @Published var mayDismiss: Bool = false
-    @Published var isThrottled: Bool = false
     @Published var config: ToastConfig
+    @Published var timer = Timer.publish(every: 0.5, on: .main, in: .common).autoconnect()
+    
+    var cancellables = Set<AnyCancellable>()
     
     /// Creates a ToastManager
     /// - Parameter config: ToastManager Configuration
@@ -26,39 +95,46 @@ public class ToastManager: ObservableObject {
     ///   - title: title of the Toast
     ///   - message: message of the Toast
     func present(_ title: String? = nil, message: String? = nil) {
+        
         DispatchQueue.main.async {
             self.shouldPresent = true
-            self.mayDismiss = false
-            self.isThrottled = true
         }
-        DispatchQueue.main.asyncAfter(deadline: .now() + config.throttle) {
-            self.isThrottled = false
-            if self.shouldPresent {
-                self.config.title = title
-                self.config.message = message
-                withAnimation {
-                    self.isPresented = true
+        
+        timer
+            .map({ (output) in
+                return output.timeIntervalSince(Date())
+            })
+            .map({ (timeInterval) in
+                return Double(timeInterval)
+            })
+            .sink { (seconds) in
+                if self.shouldPresent {
+                    if !self.isPresented, seconds >= 0.5 {
+                        DispatchQueue.main.async {
+                            self.config.title = title
+                            self.config.message = message
+                            withAnimation {
+                                self.isPresented = true
+                            }
+                        }
+                    }
+                } else {
+                    if self.isPresented {
+                        DispatchQueue.main.async {
+                            withAnimation {
+                                self.isPresented = false
+                            }
+                        }
+                    }
                 }
             }
-        }
-        DispatchQueue.main.asyncAfter(deadline: .now() + config.throttle + config.minPresentedTime) {
-            self.mayDismiss = true
-        }
+            .store(in: &cancellables)
     }
     
     /// Dismisses the Toast
     func dismiss() {
-        if isThrottled {
-            DispatchQueue.main.async {
-                self.shouldPresent = false
-            }
-        } else {
-            let minPresentedTime: Double = mayDismiss ? 0 : config.minPresentedTime
-            DispatchQueue.main.asyncAfter(deadline: .now() + minPresentedTime) {
-                withAnimation {
-                    self.isPresented = false
-                }
-            }
+        DispatchQueue.main.async {
+            self.shouldPresent = false
         }
     }
     
