eb632a1762caae08f8342ad243f39738887aaa8e
Alex Nagy <therebeloper@gmail.com>
2022-01-09 07:21:19 +0200

    DispatchQueue

--- Sources/Dot/NavigationStack/NavigationStack+Binding.swift
+++ Sources/Dot/NavigationStack/NavigationStack+Binding.swift
@@ -83,18 +83,19 @@ extension Binding {
         var animatedNavigationSteps = 0
         let pageElements = wrappedValue.pageElements.dropFirst().reversed()
         pageElements.forEach { pageElement in
+            let style = pageElement.options.style
+            if style == .sheet || style == .fullScreenCover {
+                animatedNavigationSteps += 1
+            } else {
+                if isStepped {
+                    animatedNavigationSteps += 1
+                }
+            }
             DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(wrappedValue.popMilliseconds * (animatedNavigationSteps))) {
-                let style = pageElement.options.style
                 if style == .sheet || style == .fullScreenCover {
-                    pop2(completesInstantly: false) {
-                        animatedNavigationSteps += 1
-                    }
+                    pop2(completesInstantly: false)
                 } else {
-                    pop2(completesInstantly: isStepped ? false : true) {
-                        if isStepped {
-                            animatedNavigationSteps += 1
-                        }
-                    }
+                    pop2(completesInstantly: isStepped ? false : true)
                 }
             }
         }
