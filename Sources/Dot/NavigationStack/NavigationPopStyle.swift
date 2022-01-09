dfbab0705c4ddd2a63b3cb5cb06090a3353b03e8
Alex Nagy <therebeloper@gmail.com>
2022-01-09 08:48:30 +0200

    NavigationPopStyle

---
+++ Sources/Dot/NavigationStack/NavigationPopStyle.swift
@@ -0,0 +1,14 @@
+//
+//  NavigationPopStyle.swift
+//  
+//
+//  Created by Alex Nagy on 09.01.2022.
+//
+
+import Foundation
+
+/// Represents a style for how a view should be poped.
+public enum NavigationPopStyle {
+    case automatic
+    case longest
+}
--- Sources/Dot/NavigationStack/NavigationStack+Binding.swift
+++ Sources/Dot/NavigationStack/NavigationStack+Binding.swift
@@ -43,54 +43,12 @@ extension Binding {
         }
     }
     
-    /// Pops all the ``Page``s from the ``NavigationStack``.
-    /// - Parameter completion: The closure to execute when finishing the navigation
-//    public func popToRoot<Page>(
-//        completion: @escaping () -> () = {}
-//    ) where Value == NavigationFlow<Page> {
-//        for index in 0..<wrappedValue.pageElements.count - 1 {
-//            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(wrappedValue.popMilliseconds * index)) {
-//                pop()
-//            }
-//        }
-//        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(wrappedValue.popMilliseconds * (wrappedValue.pageElements.count - 1))) {
-//            completion()
-//        }
-//    }
-    
-    /// Pops all the ``Page``s from the ``NavigationStack``.
-    /// - Parameter completion: The closure to execute when finishing the navigation
-    public func popToRoot<Page>(
-        isStepped: Bool = false,
-        completion: @escaping () -> () = {}
-    ) where Value == NavigationFlow<Page> {
-        pop(last: wrappedValue.pageElements.count - 1, isStepped: isStepped, completion: completion)
-//        var animatedNavigationSteps = 0
-//        let pageElements = wrappedValue.pageElements.dropFirst().reversed()
-//        pageElements.forEach { pageElement in
-//            let style = pageElement.options.style
-//            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(wrappedValue.popMilliseconds * (animatedNavigationSteps))) {
-//                pop()
-//            }
-//            if style == .sheet || style == .fullScreenCover {
-//                animatedNavigationSteps += 1
-//            } else {
-//                if isStepped {
-//                    animatedNavigationSteps += 1
-//                }
-//            }
-//        }
-//        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(wrappedValue.popMilliseconds * (animatedNavigationSteps))) {
-//            completion()
-//        }
-    }
-    
     /// Pops the specified last ``Page``s from the ``NavigationStack``.
     /// - Parameter last: The number of screens to be popped; default is 1
     /// - Parameter completion: The closure to execute when finishing the navigation
     public func pop<Page>(
         last: Int,
-        isStepped: Bool = false,
+        popStyle: NavigationPopStyle = .automatic,
         completion: @escaping () -> () = {}
     ) where Value == NavigationFlow<Page> {
         let pageElementsCount = wrappedValue.pageElements.count
@@ -106,7 +64,7 @@ extension Binding {
             if style == .sheet || style == .fullScreenCover {
                 animatedNavigationSteps += 1
             } else {
-                if isStepped {
+                if popStyle == .longest {
                     animatedNavigationSteps += 1
                 }
             }
@@ -116,37 +74,28 @@ extension Binding {
         }
     }
     
-    /// Pops the specified last ``Page``s from the ``NavigationStack``.
-    /// - Parameter last: The number of screens to be popped; default is 1
-    /// - Parameter completion: The closure to execute when finishing the navigation
-//    public func pop<Page>(
-//        last: Int = 1,
-//        completion: @escaping () -> () = {}
-//    ) where Value == NavigationFlow<Page> {
-//        let last = last >= wrappedValue.pageElements.count ? wrappedValue.pageElements.count - 1 : last
-//        for index in 0..<last {
-//            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(wrappedValue.popMilliseconds * index)) {
-//                self.pop()
-//            }
-//        }
-//        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(wrappedValue.popMilliseconds * last)) {
-//            completion()
-//        }
-//    }
-    
     /// Pops to the ``Page`` at the specified ``index`` in the ``NavigationStack``.
     /// - Parameter index: The index of the page to be popped to
     /// - Parameter completion: The closure to execute when finishing the navigation
     public func popTo<Page>(
         index: Int,
-        isStepped: Bool = false,
+        popStyle: NavigationPopStyle = .automatic,
         completion: @escaping () -> () = {}
     ) where Value == NavigationFlow<Page> {
         guard index < wrappedValue.pageElements.count else { return }
         let index = index >= 0 ? index : 0
         let difference = wrappedValue.pageElements.count - index - 1
         let last = difference >= wrappedValue.pageElements.count ? wrappedValue.pageElements.count - 1 : difference
-        pop(last: last, isStepped: isStepped, completion: completion)
+        pop(last: last, popStyle: popStyle, completion: completion)
+    }
+    
+    /// Pops all the ``Page``s from the ``NavigationStack``.
+    /// - Parameter completion: The closure to execute when finishing the navigation
+    public func popToRoot<Page>(
+        popStyle: NavigationPopStyle = .automatic,
+        completion: @escaping () -> () = {}
+    ) where Value == NavigationFlow<Page> {
+        popTo(index: 0, popStyle: popStyle, completion: completion)
     }
     
     /// Replaces the ``NavigationStack`` flow's current ``Page``s with a new set of ``Page``s
@@ -154,13 +103,14 @@ extension Binding {
     /// - Parameter completion: The closure to execute when finishing the navigation
     public func replaceNavigationFlow<Page>(
         newPageElements: [NavigationPageElement<Page>],
+        popStyle: NavigationPopStyle = .automatic,
         completion: @escaping () -> () = {}
     ) where Value == NavigationFlow<Page> {
-        popToRoot {
+        popToRoot(popStyle: popStyle) {
             self.wrappedValue.pageElements = []
-            for (index, screenElement) in newPageElements.enumerated() {
+            for (index, pageElement) in newPageElements.enumerated() {
                 DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(wrappedValue.popMilliseconds * index)) {
-                    self.wrappedValue.pageElements.append(screenElement)
+                    self.wrappedValue.pageElements.append(pageElement)
                 }
             }
             DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(wrappedValue.popMilliseconds * newPageElements.count)) {
