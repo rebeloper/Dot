aca8be3efce437b73c6548b59dcfd8059e101f80
Alex Nagy <therebeloper@gmail.com>
2022-02-21 13:15:31 +0200

    Space

---
+++ Sources/Dot/Utils/Space.swift
@@ -0,0 +1,14 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Alex Nagy on 21.02.2022.
+//
+
+import SwiftUI
+
+struct Space: View {
+    var body: some View {
+        Spacer(minLength: 0)
+    }
+}
