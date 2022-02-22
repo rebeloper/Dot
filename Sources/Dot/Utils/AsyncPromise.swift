62f9452479586a72eedb49d2951c8deed2794716
Alex Nagy <therebeloper@gmail.com>
2022-02-22 19:27:52 +0200

    PassthroughPromise

--- Sources/Dot/Utils/AsyncPromise.swift
+++ Sources/Dot/Utils/AsyncPromise.swift
@@ -25,22 +25,4 @@ public struct AsyncPromise<T> {
             .store(in: &cancellables)
         })
     }
-    
-    @MainActor
-    @discardableResult
-    public static func observe(_ promise: PassthroughSubject<T, Error>, storedIn cancellables: inout Set<AnyCancellable>) async throws -> T {
-        try await withCheckedThrowingContinuation({ continuation in
-            promise.sink { result in
-                switch result {
-                case .finished:
-                    break
-                case .failure(let err):
-                    continuation.resume(throwing: err)
-                }
-            } receiveValue: { value in
-                continuation.resume(returning: value)
-            }
-            .store(in: &cancellables)
-        })
-    }
 }
