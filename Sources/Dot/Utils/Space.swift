045f28b957847fbc14444059ac1423c890afeaf7
Alex Nagy <therebeloper@gmail.com>
2022-02-23 11:58:12 +0200

    Space

--- Sources/Dot/Utils/Space.swift
+++ Sources/Dot/Utils/Space.swift
@@ -9,9 +9,39 @@ import SwiftUI
 
 public struct Space: View {
     
-    public init() {}
+    private var width: CGFloat?
+    private var height: CGFloat?
+    
+    /// ``Spacer`` with ``minLength`` of ``0`` and width & height
+    public init(width: CGFloat,
+                height: CGFloat) {
+        self.width = width
+        self.height = height
+    }
+    
+    /// ``Spacer`` with ``minLength`` of ``0`` and a width
+    public init(width: CGFloat) {
+        self.width = width
+        self.height = nil
+    }
+    
+    /// ``Spacer`` with ``minLength`` of ``0`` and a height
+    public init(height: CGFloat) {
+        self.width = nil
+        self.height = height
+    }
+    
+    /// ``Spacer`` with ``minLength`` of ``0``
+    public init() {
+        self.width = nil
+        self.height = nil
+    }
     
     public var body: some View {
-        Spacer(minLength: 0)
+        if width == nil, height == nil {
+            Spacer(minLength: 0)
+        } else {
+            Spacer(minLength: 0).frame(width: width, height: height)
+        }
     }
 }
