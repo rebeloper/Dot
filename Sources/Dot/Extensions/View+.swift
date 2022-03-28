40deaaafe5c5a6f84d2283870f6dd802a91bf63a
Alex Nagy <therebeloper@gmail.com>
2022-03-28 13:12:01 +0300

    hideKeyboard

--- Sources/Dot/Extensions/View+.swift
+++ Sources/Dot/Extensions/View+.swift
@@ -205,4 +205,10 @@ public extension View {
             }
             .background(content())
     }
+    
+    #if canImport(UIKit)
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+    #endif
 }
