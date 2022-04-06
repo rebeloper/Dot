2dbb224d943987dd095f0c44aef22fbca0438d7f
Alex Nagy <therebeloper@gmail.com>
2022-04-06 14:03:41 +0300

    spacing

--- Sources/Dot/Views/FlexibleView.swift
+++ Sources/Dot/Views/FlexibleView.swift
@@ -10,11 +10,24 @@ import SwiftUI
 /// Facade of our view, its main responsibility is to get the available width
 /// and pass it down to the real implementation, `_FlexibleView`.
 public struct FlexibleView<Data: Collection, Content: View>: View where Data.Element: Hashable {
-    public let data: Data
-    public let spacing: CGFloat
-    public let alignment: HorizontalAlignment
-    public let content: (Data.Element) -> Content
+    let data: Data
+    let vSpacing: CGFloat
+    let hSpacing: CGFloat
+    let alignment: HorizontalAlignment
+    let content: (Data.Element) -> Content
     @State private var availableWidth: CGFloat = 0
+    
+    public init(data: Data,
+                vSpacing: CGFloat = 12,
+                hSpacing: CGFloat = 12,
+                alignment: HorizontalAlignment = .leading,
+                content: @escaping (Data.Element) -> Content) {
+        self.data = data
+        self.vSpacing = vSpacing
+        self.hSpacing = hSpacing
+        self.alignment = alignment
+        self.content = content
+    }
     
     public var body: some View {
         ZStack(alignment: Alignment(horizontal: alignment, vertical: .center)) {
@@ -27,7 +40,8 @@ public struct FlexibleView<Data: Collection, Content: View>: View where Data.Ele
             _FlexibleView(
                 availableWidth: availableWidth,
                 data: data,
-                spacing: spacing,
+                vSpacing: vSpacing,
+                hSpacing: hSpacing,
                 alignment: alignment,
                 content: content
             )
@@ -40,15 +54,16 @@ public struct FlexibleView<Data: Collection, Content: View>: View where Data.Ele
 struct _FlexibleView<Data: Collection, Content: View>: View where Data.Element: Hashable {
     let availableWidth: CGFloat
     let data: Data
-    let spacing: CGFloat
+    let vSpacing: CGFloat
+    let hSpacing: CGFloat
     let alignment: HorizontalAlignment
     let content: (Data.Element) -> Content
     @State var elementsSize: [Data.Element: CGSize] = [:]
     
     var body : some View {
-        VStack(alignment: alignment, spacing: spacing) {
+        VStack(alignment: alignment, spacing: vSpacing) {
             ForEach(computeRows(), id: \.self) { rowElements in
-                HStack(spacing: spacing) {
+                HStack(spacing: hSpacing) {
                     ForEach(rowElements, id: \.self) { element in
                         content(element)
                             .fixedSize()
@@ -69,7 +84,7 @@ struct _FlexibleView<Data: Collection, Content: View>: View where Data.Element: 
         for element in data {
             let elementSize = elementsSize[element, default: CGSize(width: availableWidth, height: 1)]
             
-            if remainingWidth - (elementSize.width + spacing) >= 0 {
+            if remainingWidth - (elementSize.width + hSpacing) >= 0 {
                 rows[currentRow].append(element)
             } else {
                 currentRow = currentRow + 1
@@ -77,7 +92,7 @@ struct _FlexibleView<Data: Collection, Content: View>: View where Data.Element: 
                 remainingWidth = availableWidth
             }
             
-            remainingWidth = remainingWidth - (elementSize.width + spacing)
+            remainingWidth = remainingWidth - (elementSize.width + hSpacing)
         }
         
         return rows
