551e4414de47cc270913f11802632e6b643d7f25
Alex Nagy <therebeloper@gmail.com>
2022-04-06 13:52:59 +0300

    public

--- Sources/Dot/Views/FlexibleView.swift
+++ Sources/Dot/Views/FlexibleView.swift
@@ -9,14 +9,14 @@ import SwiftUI
 
 /// Facade of our view, its main responsibility is to get the available width
 /// and pass it down to the real implementation, `_FlexibleView`.
-struct FlexibleView<Data: Collection, Content: View>: View where Data.Element: Hashable {
-    let data: Data
-    let spacing: CGFloat
-    let alignment: HorizontalAlignment
-    let content: (Data.Element) -> Content
+public struct FlexibleView<Data: Collection, Content: View>: View where Data.Element: Hashable {
+    public let data: Data
+    public let spacing: CGFloat
+    public let alignment: HorizontalAlignment
+    public let content: (Data.Element) -> Content
     @State private var availableWidth: CGFloat = 0
     
-    var body: some View {
+    public var body: some View {
         ZStack(alignment: Alignment(horizontal: alignment, vertical: .center)) {
             Color.clear
                 .frame(height: 1)
